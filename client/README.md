b9123dc79928404177ffdce032eaa12a4bead348
王开朗 <wangkailang992@gmail.com>
2019-02-26 23:00:09 +0800

    add client

---
+++ client/README.md
@@ -0,0 +1,7 @@
+```bash
+npx create-react-app client
+npm install apollo-boost react-apollo graphql --save
+```
+[link server](https://www.apollographql.com/docs/react/essentials/get-started.html)
+
+参考： https://github.com/graphql-boilerplates
---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "react-apollo";
+import UsersTable from './UsersTable';
+import './App.css';
+
+const client = new ApolloClient ({
+  uri: 'http://localhost:4466',
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <h2>Appllo App</h2>
+          <UsersTable />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
