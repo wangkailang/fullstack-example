94c8afc123ad2dd57a26b7dee39a4c517d537930
王开朗 <wangkailang992@gmail.com>
2019-02-27 16:37:46 +0800

    feat: add component mode code and validate

--- client/src/UsersTable/index.js
+++ client/src/UsersTable/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { compose, graphql } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Table, Button, Modal, Form, Row, Col } from 'react-bootstrap';
+
 const GET_USERS = gql`{
   users {
     id
     name
   }
-}`
-
+}`;
 const CREATE_USER = gql`
   mutation CreateUser($data: UserCreateInput!) {
     createUser(data: $data) {
@@ -17,7 +17,7 @@ const CREATE_USER = gql`
       email,
     }
   }
-`
+`;
 const DELETE_USER = gql`
   mutation DeleteUser($where: UserWhereUniqueInput!) {
     deleteUser(where: $where) {
@@ -26,14 +26,7 @@ const DELETE_USER = gql`
   }
 `;
 
-
-const connector = compose(
-  graphql(GET_USERS),
-  graphql(CREATE_USER),
-  graphql(DELETE_USER),
-)
-
-class UsersTable extends React.PureComponent {
+class UsersTable extends React.Component {
   state = {
     show: false,
     name: '',
@@ -49,86 +42,121 @@ class UsersTable extends React.PureComponent {
       name: event.target.value
     })
   }
-  handleSubmit = async (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    await this.props.mutate({
-      variables:{
-        data: {
-          name:  this.state.name
-        }
-      }
-    })
-    await this.props.data.refetch();
-    this.handleClose();
-  }
-  delete = id => {
-    return async () => {
-      await this.props.mutate({
-      variables:{
-        where: {
-          id
-        }
-      }
-    })
-    await this.props.data.refetch();
-    }
-  }
 
-  render() {
-    const { data: { loading, error, users } } = this.props;
-    console.log(this.props);
-    if (loading) return <p>loading...</p>;
-    if (error) return <p>Error: {error}</p>;
+  render() {  
     return (
-      <div>
-        <Table responsive>
-          <thead>
-            <tr>
-              <td>ID</td>
-              <td>Name</td>
-              <td>Delete</td>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(({ id, name }) => (
-              <tr key={id}>
-                <td>{id}</td>
-                <td>{name}</td>
-                <td><Button variant="danger" onClick={this.delete(id)}>Delete</Button></td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-        <div>
-          <Button onClick={this.handleShow}>Add+</Button>
-        </div>
-        <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Add User</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form noValidate onSubmit={this.handleSubmit}>
-              <Form.Group as={Row} controlId="user">
-                <Form.Label column sm="2">Name:</Form.Label>
-                <Col sm="10">
-                  <Form.Control plaintext placeholder="Enter your name" onChange={this.handleNameChange}/>
-                </Col>
-              </Form.Group>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={this.handleClose}>
-                  Close
-                </Button>
-                <Button type="submit">
-                  Add
-                </Button>
-              </Modal.Footer>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      </div>
+      <Query query={GET_USERS}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>loading...</p>;
+          if (error) return <p>Error: {error}</p>;
+          return (
+            <div>
+              <Table responsive>
+                <thead>
+                  <tr>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Delete</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.users.map(({ id, name }) => (
+                    <tr key={id}>
+                      <td>{id}</td>
+                      <td>{name}</td>
+                      <td>
+                        <Mutation
+                          mutation={DELETE_USER}
+                          update={(cache, { data: { deleteUser } }) => {
+                            const { users } = cache.readQuery({ query:  GET_USERS});
+                            cache.writeQuery({
+                              query: GET_USERS,
+                              data: { users: users.filter(user => user.id !== deleteUser.id) }
+                            })
+                          }}
+                        >
+                          {deleteUser => (
+                            <Button
+                              variant="danger"
+                              onClick={async () => {
+                                deleteUser({
+                                  variables: {
+                                    where: { id }
+                                  }
+                                })
+                              }}
+                            >Delete</Button>
+                          )}
+                        </Mutation>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+              <div>
+                <Button onClick={this.handleShow}>Add+</Button>
+              </div>
+              <Modal show={this.state.show} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Add User</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Mutation
+                    mutation={CREATE_USER}
+                    update={(cache, { data: { createUser } }) => {
+                      const { users } = cache.readQuery({ query:  GET_USERS});
+                      cache.writeQuery({
+                        query: GET_USERS,
+                        data: { users: users.concat([createUser]) }
+                      })
+                    }}
+                  >
+                    {(createUser) =>(
+                      <Form validated onSubmit={async event => {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        await createUser({
+                          variables: {
+                            data: {
+                              name: this.state.name
+                            }
+                          }
+                        })
+                        this.handleClose();
+                      }}>
+                        <Form.Group as={Row} controlId="user">
+                          <Form.Label column sm="2">Name:</Form.Label>
+                          <Col sm="10">
+                            <Form.Control
+                              required
+                              type="text"
+                              placeholder="Enter your name"
+                              onChange={this.handleNameChange}
+                            />
+                          </Col>
+                          <Form.Control.Feedback type="invalid">
+                            Please provide a valid name.
+                          </Form.Control.Feedback>
+                        </Form.Group>
+                        <Modal.Footer>
+                          <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                          </Button>
+                          <Button type="submit">
+                            Add
+                          </Button>
+                        </Modal.Footer>
+                      </Form>
+                    )}
+                  </Mutation>
+                </Modal.Body>
+              </Modal>
+            </div>
+          )
+        }}
+      </Query>
     )
   }
 }
 
-export default connector(UsersTable);
+export default UsersTable;
